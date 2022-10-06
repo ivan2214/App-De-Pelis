c9ae3606a48444bfe8a729c8d54881c66312e094
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-10-06 13:10:05 -0300

    adding to chakra
    añadiendo chakra iu para emepzar a dar estilso mas adelante

---
+++ src/config/theme.js
@@ -0,0 +1,27 @@
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        margin: 0,
+        padding: 0,
+        boxSizing: "border-box",
+        bg: "gray.400",
+        color: "white",
+      },
+      // styles for the `a`
+      a: {
+        color: "teal.500",
+        _hover: {
+          textDecoration: "underline",
+        },
+      },
+    },
+  },
+});
+
+export default theme
